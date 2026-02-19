12e342b732daf8c18c33cac06bc303d4f46faf69
jethermasidong <jethermasidong05@gmail.com>
2026-02-19 20:53:49 +0800

    Progress: Created Controller and Defining Movies

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function movies()
+    {
+        $movies = [
+            'Top Gun Maverick',
+            '1917 Movie',
+            'Wreck it Ralph',
+            'The Notebook',
+            'The Shawhank Redemption',
+            'The Godfather',
+            'Toy Story',
+            'Godzilla Minus One',
+            'How to train your dragon',
+            'Sinners',
+            'The Wrestler',
+            'The Father'
+        ];
+        return view('movie', ['movies' => $movies ]);
+    }
 }
